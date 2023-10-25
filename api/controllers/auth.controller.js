a3e56738100cf5d4fd1d79c2bf87edb6842239a5
A-karkaih <akarkaih@gmail.com>
2023-10-25 17:01:16 +0100

    cretae a middleware and a function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,24 @@
 import User from '../models/user.model.js';
 import bcrytjs from 'bcryptjs';
-export const signUp = async (req, res) => {
+export const signUp = async (req, res , next) => {
     const { username, email, password } = req.body;
     const hashedpassword = bcrytjs.hashSync(password, 10);
-    const newUser = new  User({
+    const newUser = new User({
         username,
         email,
-        password : hashedpassword
+        password: hashedpassword
     });
    
     try {
+        
         await newUser.save();
 
+        res.status(201).json("User created succefully");
+
     } catch (err) {
-        res.status(500).json(err.message);
-}
+        next(err);
+    }
 
-    res.status(201).json("User created succefully");
+    
 
-}
+};
