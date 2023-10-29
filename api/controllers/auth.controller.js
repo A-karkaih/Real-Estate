82464ba7522e743abc98dfffb904e290baefb8cb
A-karkaih <akarkaih@gmail.com>
2023-10-29 17:25:21 +0100

    update header and make profile page private

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -52,13 +52,12 @@ export const signIn = async (req, res, next) => {
 
 };
 
-
 export const google = async (req, res, next) => {
     
     try {
        
         const user = await User.findOne({ email: req.body.email });
-     
+        
         if (user) {
 
             const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
@@ -93,6 +92,7 @@ export const google = async (req, res, next) => {
         next(error);
         
     }
+
     
 
 }
