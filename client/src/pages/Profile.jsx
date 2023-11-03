c58a4421557e1f0fcd93ed7ca7992d36487375fa
A-karkaih <akarkaih@gmail.com>
2023-11-03 17:57:29 +0100

    complete user listing delete

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -151,6 +151,25 @@ const Profile = () => {
       setShowError(true);
     }
   };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
@@ -286,8 +305,13 @@ const Profile = () => {
               </Link>
 
               <div className="flex flex-col item-center">
-                <button  className="text-red-700 uppercase">Delete</button>
-                <button  className="text-green-700 uppercase">Edit</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <button className="text-green-700 uppercase">Edit</button>
               </div>
             </div>
           ))}
