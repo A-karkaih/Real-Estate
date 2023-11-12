84b59cd773ccf40f2fb6443c83645b47a8e1fc0b
A-karkaih <akarkaih@gmail.com>
2023-11-12 16:45:16 +0100

    finishing home page and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,10 +2,34 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Sahand Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to Achraf Estate, a premier real estate agency dedicated to
+        facilitating your property transactions seamlessly. Specializing in
+        assisting clients with buying, selling, and renting properties in highly
+        sought-after neighborhoods, our seasoned team of agents is committed to
+        delivering exceptional service.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At Achraf Estate, our mission is centered around helping you achieve
+        your real estate goals. We provide expert advice, personalized service,
+        and a comprehensive understanding of the local market dynamics. Whether
+        you're in the market to buy, sell, or rent a property, our team is here
+        to guide you through every step of the process.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our agents bring a wealth of experience and knowledge to the table,
+        ensuring that you receive the highest level of service. We believe that
+        the journey of buying or selling a property should be both exciting and
+        rewarding. With Achraf Estate, we are devoted to turning that belief
+        into a reality for each of our valued clients. Trust us to make your
+        real estate experience truly remarkable.
+      </p>
     </div>
-  )
+  );
 }
 
 export default About;
